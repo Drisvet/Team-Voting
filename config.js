7ccef223c945aeec4d0cec1263323a4d25641e85
Drisvet <71675411+Drisvet@users.noreply.github.com>
2022-04-27 03:04:49 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,9 +2,18 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    const firebaseConfig = {
+      apiKey: "AIzaSyAMpAVISTbIbpRemY9TPTF9qD2wX2-w5NY",
+      authDomain: "team-voting-ef224.firebaseapp.com",
+      projectId: "team-voting-ef224",
+      storageBucket: "team-voting-ef224.appspot.com",
+      messagingSenderId: "491867567353",
+      appId: "1:491867567353:web:194c0f010d5aac4ae71e28"
+    };
+    
+ // Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+;
 
 export default firebase.database();
